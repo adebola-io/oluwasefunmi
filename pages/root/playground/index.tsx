8d5075077b9cede6e72ed8c786ac6d2105bc428a
adebola-io <adebolaakomolafe@gmail.com>
2025-03-21 17:47:57 +0100

    feat: Improve playground page layout
    This commit enhances the layout of the playground page:
    
    -   **Page Component:**
        -   Added `grid-rows-[auto_auto_1fr]` to the `playground` page to improve its layout.

--- pages/root/playground/index.tsx
+++ pages/root/playground/index.tsx
@@ -4,7 +4,7 @@ import { LargeText } from '@/components/typography';
 
 const Playground: RouteComponent<PageMeta> = () => {
   return (
-    <div class="grid py-6 relative top-level-bare">
+    <div class="grid grid-rows-[auto_auto_1fr] py-6 relative top-level-bare">
       <LargeText underline class="mb-1">
         playground.
       </LargeText>
