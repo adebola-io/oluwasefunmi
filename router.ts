78af270c3eeb7997f7c6fd467ad4302b0db32e97
adebola-io <adebolaakomolafe@gmail.com>
2025-04-22 20:00:25 +0100

    fix: Export context from retend/context from router.

--- router.ts
+++ router.ts
@@ -2,6 +2,8 @@ import { createWebRouter } from 'retend/router';
 import { waitingRoute } from './pages/waiting/routes';
 import { rootRoutes, sidebarAvoidanceMiddleware } from './pages/root/routes';
 
+export * as context from 'retend/context';
+
 export function createRouter() {
   return createWebRouter({
     routes: [waitingRoute, rootRoutes],
