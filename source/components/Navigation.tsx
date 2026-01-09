9fdb52b5bf2f56d74321afd4c3c7dcbf53996aa1
Sefunmi <adebolaakomolafe@gmail.com>
2026-01-09 23:13:39 +0100

    update styles

--- source/components/Navigation.tsx
+++ source/components/Navigation.tsx
@@ -1,20 +1,53 @@
-import { Link } from "retend/router";
+import { Cell } from "retend";
+import { useRouter, Link } from "retend/router";
 import classes from "./Navigation.module.css";
 
 export const Navigation = () => {
+  const isMenuOpen = Cell.source(false);
+  const router = useRouter();
+
+  // Close menu whenever route changes
+  router.getCurrentRoute().listen(() => {
+    isMenuOpen.set(false);
+  });
+
+  const toggleMenu = () => isMenuOpen.set(!isMenuOpen.get());
+  const closeMenu = () => isMenuOpen.set(false);
+
   return (
     <nav class={classes.nav}>
-      <Link href="/" class={classes.logo}>
+      <Link href="/" class={classes.logo} onClick={closeMenu}>
         oluwasefunmi
       </Link>
-      <div class={classes.navLinks}>
-        <Link href="/explorations" class={classes.navLink}>
+
+      <button
+        class={classes.menuToggle}
+        onClick={toggleMenu}
+        aria-label="Toggle menu"
+      >
+        <span class={classes.bar}></span>
+        <span class={classes.bar}></span>
+      </button>
+
+      <div
+        class={[classes.backdrop, { [classes.backdropOpen]: isMenuOpen }]}
+        onClick={closeMenu}
+      ></div>
+
+      <div class={[classes.navLinks, { [classes.navLinksOpen]: isMenuOpen }]}>
+        <div class={classes.sidebarHeader}>
+          <span class={classes.sidebarTitle}>Menu</span>
+          <button class={classes.closeButton} onClick={closeMenu}>
+            &times;
+          </button>
+        </div>
+        <Link href="/explorations" class={classes.navLink} onClick={closeMenu}>
           explorations
         </Link>
-        <Link href="/contact" class={classes.navLink}>
+        <Link href="/contact" class={classes.navLink} onClick={closeMenu}>
           contact
         </Link>
-        <Link href="/random-notes" class={classes.navLink}>
+        <Link href="/random-notes" class={classes.navLink} onClick={closeMenu}>
           random notes
         </Link>
       </div>
