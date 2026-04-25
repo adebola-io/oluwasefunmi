cd4d0439351728f87223ee8c99ed0da7d95466fe
Sefunmi <adebolaakomolafe@gmail.com>
2026-04-25 08:48:57 +0100

    move albums to json

--- source/features/playground/experiments/music-details/AlbumCover.tsx
+++ source/features/playground/experiments/music-details/AlbumCover.tsx
@@ -11,25 +11,25 @@ export function AlbumCover(props: AlbumCoverProps) {
   return (
     <Link
       href="#"
-      class="grid grid-cols-1 group perspective-midrange rounded-lg"
+      class="grid grid-cols-1 group perspective-midrange rounded-lg size-full"
     >
       <div
         data-back
-        class="z-[-1] w-auto aspect-square bg-white rounded-lg overflow-hidden outline-1 outline-[#ffffff70]"
-        style={{ gridArea: "1/1" }}
+        class="z-[-1] size-full aspect-square rounded-lg overflow-hidden outline-1 outline-[#ffffff70]"
+        style={{ gridArea: "1/1", backgroundColor: album.themeColor }}
       />
       <div
         data-front
         class={[
-          "w-auto aspect-square rounded-lg overflow-hidden outline-1 outline-[#ffffff70]",
+          "size-full aspect-square rounded-lg overflow-hidden outline-1 outline-[#ffffff70]",
           "duration-350 ease-(--ease-spring) transition-transform perspective-origin-left origin-left",
         ]}
-        style={{ gridArea: "1/1" }}
+        style={{ gridArea: "1/1", backgroundColor: album.themeColor }}
       >
         <img
           src={album.imageUrl}
           alt={`${album.name} by ${album.artist}`}
-          class="h-full object-cover"
+          class="size-full object-cover"
         />
       </div>
     </Link>
